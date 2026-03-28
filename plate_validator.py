b7e383d3e0229d337268f5c14deef3a70b0dc660
PrincetonAfeez <newlifewithprince@live.com>
2026-03-27 20:48:34 -0700

    import re, json, sys, console and panel

---
+++ plate_validator.py
@@ -0,0 +1,8 @@
+import re # Library for Regular Expression matching
+import json # Library for loading regional patterns
+import sys # Library for system operations
+from rich.console import Console # UI Library
+from rich.panel import Panel # UI Layout
+
+console = Console() # Initialize the console
+
